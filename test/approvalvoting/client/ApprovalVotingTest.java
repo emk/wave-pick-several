a2fe97f3649e4bc620ec2e60d4805486e59496ff
Eric Kidd <git@randomhacks.net>
2009-09-14 19:53:12 -0400

    Add automatically-generated test stubs

---
+++ test/approvalvoting/client/ApprovalVotingTest.java
@@ -0,0 +1,24 @@
+package approvalvoting.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * GWT JUnit tests must extend GWTTestCase.
+ */
+public class ApprovalVotingTest extends GWTTestCase {
+
+  /**
+   * Must refer to a valid module that sources this class.
+   */
+  public String getModuleName() {
+    return "approvalvoting.ApprovalVoting";
+  }
+
+  /**
+   * Add as many tests as you like.
+   */
+  public void testSimple() {
+    assertTrue(true);
+  }
+
+}
