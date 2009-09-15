5321dacda4c30778015bcb697be062ebc658bcd9
Eric Kidd <git@randomhacks.net>
2009-09-14 22:48:33 -0400

    Define a test suite which runs all our tests

---
+++ test/approvalvoting/ApprovalVotingTestSuite.java
@@ -0,0 +1,16 @@
+package approvalvoting;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import approvalvoting.client.ApprovalVotingTest;
+
+import com.google.gwt.junit.tools.GWTTestSuite;
+
+public class ApprovalVotingTestSuite extends GWTTestSuite {
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for ApprovalVoting gadget");
+		suite.addTestSuite(ApprovalVotingTest.class);
+		return suite;
+	}
+}
