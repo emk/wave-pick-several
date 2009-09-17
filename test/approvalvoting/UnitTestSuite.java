ac858df597ea5ae97f9e89c90a9afd3fc12cd2e8
Eric Kidd <git@randomhacks.net>
2009-09-16 21:33:45 -0400

    Remove unused code
    This code (and the associated launchers, etc), is no usable now that we
    have converted this project to a gadget.

--- test/approvalvoting/UnitTestSuite.java
+++ test/approvalvoting/UnitTestSuite.java
@@ -5,7 +5,7 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 /** This test suite should only include non-browser tests. */
-public class FastTestSuite extends TestSuite {
+public class UnitTestSuite extends TestSuite {
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Fast tests");
 		suite.addTestSuite(ChoiceTest.class);
